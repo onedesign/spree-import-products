94ca3e6caff9affa40f1d0b6c94c4c2ca15326b4
AJ Schuster <ajs@maximumaardvark.com>
2012-06-22 15:53:37 -0500

    Fix a silly error that caused this admin tab to collide with the spree_promo admin tab.

--- app/overrides/import_admin_tabs.rb
+++ app/overrides/import_admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "promo_admin_tabs",
+                     :name => "product_imports_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:product_imports) %>",
                      :disabled => false)
